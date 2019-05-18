47630e172db1576584494797f4e2a75decfd95ce
eugene <1160178908@qq.com>
2019-05-18 10:21:08 +0800

    0518

---
+++ miniprogram/data/db.js
@@ -0,0 +1,40 @@
+var articles = [
+  {
+    avatar: '/images/wyc.jpg',
+    author:'王翌宸',
+    date: '2019-05-07',
+    time: '3天前',
+    title: '王翌宸是我',
+    img: '/images/swiper/wyc3.jpg',
+    desc: '富士山下',
+    content:'我是内容1',
+    star: 30,
+    view: '40',
+    articleId:'0',
+    music:{
+      src: 'http://sc1.111ttt.cn:8282/2017/1/11/11/304112003137.mp3?tflag=1546606800&pin=97bb2268ae26c20fe093fd5b0f04be80',
+      title: '远走高飞'
+    }
+  },
+  {
+    avatar: '/images/wyc.jpg',
+    author: '王翌宸',
+    date: '2019-05-08',
+    time:'4天前',
+    title: '双叶幼稚园某之助',
+    img: '/images/swiper/wyc2.jpg',
+    desc: '樱花盛开',
+    content: '我是内容2',
+    star: 40,
+    view: '50',
+    articleId: '1',
+    music: {
+      src: 'http://sc1.111ttt.cn:8282/2018/1/03/13/396131227447.mp3?tflag=1546606800&pin=97bb2268ae26c20fe093fd5b0f04be80',
+      title: '红昭愿'
+    }
+  }
+];
+
+module.exports = {
+  articles: articles
+}
